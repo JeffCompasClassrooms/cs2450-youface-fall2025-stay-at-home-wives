3c265cafedca0228e95dabc026b9f9b568623ead
nop256 <N0P256@proton.me>
2025-10-21 13:32:26 -0600

    restructured

--- db/helpers.py
+++
@@ -1,4 +0,0 @@
-import tinydb
-
-def load_db():
-    return tinydb.TinyDB('db.json', sort_keys=True, indent=4, separators=(',', ': '))
--- handlers/copy.py
+++
@@ -1,2 +0,0 @@
-title = 'YouFace 2.0'
-subtitle = "A billion dollars and it's yours!"
