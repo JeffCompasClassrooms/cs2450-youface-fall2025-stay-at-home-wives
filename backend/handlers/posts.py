6b0a1ea249a363e6111f3d1db3f2f091c6ac38bf
nop256 <n0p256@proton.me>
2025-10-27 09:36:14 -0600

    fixed post handler

--- backend/handlers/posts.py
+++ backend/handlers/posts.py
@@ -39,8 +39,7 @@ def view_post(post_id):
         return ("Post not found", 404)
 
     posts_db.increment_views(db, post_id)
-    return p
-    #return flask.render_template('posts.html', post=p, username=username, user=user)
+    return flask.render_template('posts.html', post=p, username=username, user=user)
 
 @blueprint.post('/posts/<int:post_id>/comment')
 def add_comment(post_id):
