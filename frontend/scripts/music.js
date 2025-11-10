dbf115e8ca39312f74d55408ad4b99a11e5ef981
nop256 <n0p256@proton.me>
2025-11-10 13:15:46 -0700

    rollback

--- frontend/scripts/music.js
+++
@@ -1,7 +0,0 @@
-let waves = new Audio("css/assets/sounds/" + wavesSound);
-waves.volume = 0.4;
-waves.loop = true;
-let playing = false;
-function startWaves() {
-    if (playing !== (playing = true)) waves.play()
-}
